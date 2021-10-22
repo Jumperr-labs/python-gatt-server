159c395aaf933a1e167a9a4a288399df901aa819
Rida Shamasneh <ridahani@gmail.com>
2021-10-22 22:44:42 +0300

    show current angles on view

--- cube_3d.py
+++ cube_3d.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from PyQt5 import QtCore  # core Qt functionality
 import sys  # we'll need this later to run our Qt application
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QSlider, QApplication
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QSlider, QApplication, QLabel, QHBoxLayout, QGridLayout
 
 from gl_widget import GLWidget
 import dbus
@@ -69,42 +69,64 @@ class MainWindow(QMainWindow):
 
     def init_gui(self):
         central_widget = QWidget()
-        gui_layout = QVBoxLayout()
+        gui_layout = QGridLayout()
         central_widget.setLayout(gui_layout)
-
         self.setCentralWidget(central_widget)
 
-        gui_layout.addWidget(self.glWidget)
-
+        x_widget = QWidget()
+        x_layout = QHBoxLayout()
+        x_layout.setContentsMargins(0, 0, 0, 0)
+        x_widget.setLayout(x_layout)
+        self.x_label = QLabel("Pitch 0")
+        x_layout.addWidget(self.x_label)
         slider_x = QSlider(QtCore.Qt.Horizontal)
+        x_layout.addWidget(slider_x)
         slider_x.valueChanged.connect(self.slider_x_valueChanged_handler)
         slider_x.setMinimum(-180)
         slider_x.setMaximum(180)
 
+        y_widget = QWidget()
+        y_layout = QHBoxLayout()
+        y_layout.setContentsMargins(0, 0, 0, 0)
+        y_widget.setLayout(y_layout)
+        self.y_label = QLabel("Yaw 0")
+        y_layout.addWidget(self.y_label)
         slider_y = QSlider(QtCore.Qt.Horizontal)
+        y_layout.addWidget(slider_y)
         slider_y.valueChanged.connect(self.slider_y_valueChanged_handler)
         slider_y.setMinimum(-180)
         slider_y.setMaximum(180)
 
+        z_widget = QWidget()
+        z_layout = QHBoxLayout()
+        z_layout.setContentsMargins(0, 0, 0, 0)
+        z_widget.setLayout(z_layout)
+        self.z_label = QLabel("Roll 0")
+        z_layout.addWidget(self.z_label)
         slider_z = QSlider(QtCore.Qt.Horizontal)
+        z_layout.addWidget(slider_z)
         slider_z.valueChanged.connect(self.slider_z_valueChanged_handler)
         slider_z.setMinimum(-180)
         slider_z.setMaximum(180)
 
-        gui_layout.addWidget(slider_x)
-        gui_layout.addWidget(slider_y)
-        gui_layout.addWidget(slider_z)
-    
+        gui_layout.addWidget(self.glWidget, 0, 0, 10, 1)
+        gui_layout.addWidget(x_widget, 11, 0, 1, 1)
+        gui_layout.addWidget(y_widget, 12, 0, 1, 1)
+        gui_layout.addWidget(z_widget, 13, 0, 1, 1)
+
     def slider_x_valueChanged_handler(self, val):
         self.glWidget.setRotX(val)
+        self.x_label.setText(f"Pitch {val}")
         self.update_simulator()
 
     def slider_y_valueChanged_handler(self, val):
         self.glWidget.setRotY(val)
+        self.y_label.setText(f"Yaw {val}")
         self.update_simulator()
 
     def slider_z_valueChanged_handler(self, val):
         self.glWidget.setRotZ(val)
+        self.z_label.setText(f"Roll {val}")
         self.update_simulator()
 
     def init_and_run_simulator(self):
@@ -112,7 +134,7 @@ class MainWindow(QMainWindow):
         self.simulator.start()
     
     def update_simulator(self):
-        self.simulator.update_rpy(self.glWidget.getRotX(), self.glWidget.getRotY(), self.glWidget.getRotZ())
+        self.simulator.update_rpy(self.glWidget.getRotZ(), self.glWidget.getRotX(), self.glWidget.getRotY())
 
     def cleanup(self):
         self.simulator.destroy()
